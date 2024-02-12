70cbebf5eac301a317a11561d7c9b01e2cb06a14
prashanna17 <thapapra@sheridancollege.ca>
2024-02-12 13:38:13 -0500

    prashanna thapa

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -1,36 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arithmetic;
 
-
 import java.util.Scanner;
-import static java.time.Clock.system;
 
-/** This class calls the method to perform 
- * arithmetic operations based on user input
- * execute the code check the output
- * @author sivagamasrinivasan
- * 
- */
-public class Arithmetic 
-{
+public class Arithmetic {
+    public static void main(String[] args) {
+        ArithmeticBase r = new ArithmeticBase();
+        Scanner in = new Scanner(System.in);
+        double m = in.nextDouble();
+        double n = in.nextDouble();
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+        System.out.println("Enter arithmetic operation to Perform (PLUS, MINUS, TIMES, DIVIDE): ");
+        String input = in.next().toUpperCase();
+        ArithmeticBase.Operation operation = ArithmeticBase.Operation.valueOf(input);
+
+        double result = r.calculate(m, n, operation);
+        System.out.println("Result: " + result);
     }
 }
-
--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -1,39 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arithmetic;
 
 import java.util.Scanner;
 
-/** This class takes String input plus,minus,divide and times
- * from user and execute the arithmetic operation
- * change the code to use enum instead String and mention the advantage of enum.
- * @author sivagamasrinivasan
- * 
- */
-public class ArithmeticBase 
-{
- public double x,y;
-    double calculate(double x, double y) 
-        {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter arithmetic operation to Perform: ");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
-        {
-            case "PLUS":
+public class ArithmeticBase {
+    public double x, y;
+
+    enum Operation {
+        PLUS,
+        MINUS,
+        TIMES,
+        DIVIDE
+    }
+
+    double calculate(double x, double y, Operation operation) {
+        switch (operation) {
+            case PLUS:
                 return x + y;
-            case "MINUS":
+            case MINUS:
                 return x - y;
-            case "TIMES":
+            case TIMES:
                 return x * y;
-            case "DIVIDE":
+            case DIVIDE:
                 return x / y;
             default:
-                throw new AssertionError("Unknown operations " + this);
+                throw new AssertionError("Unknown operation " + operation);
         }
     }
-   
+
+    public static void main(String[] args) {
+        ArithmeticBase arithmetic = new ArithmeticBase();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two numbers: ");
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        System.out.println("Enter arithmetic operation to Perform (PLUS, MINUS, TIMES, DIVIDE): ");
+        String input = sc.next().toUpperCase();
+        Operation operation = Operation.valueOf(input);
+
+        double result = arithmetic.calculate(x, y, operation);
+        System.out.println("Result: " + result);
+    }
 }
